9065a7903b89adbe6aa040b45b916fe3287df175
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2017-10-14 13:32:57 -0700

    q 48 rotate image

---
+++ src/main/java/jalgor_gen2/leetcode/Q048_RotateImage.java
@@ -0,0 +1,25 @@
+package jalgor_gen2.leetcode;
+
+import java.util.Arrays;
+
+public class Q048_RotateImage extends Solution {
+   
+	public void rotate(int[][] matrix) {
+		
+		int n = matrix.length;
+		
+		for ( int i = 0; i < n/2; i ++)
+			for ( int j = 0; j < n; j++) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[n-i-1][j];
+				matrix[n-i-1][j] = temp;
+			}
+		
+		for ( int i = 0; i < n; i ++ )
+			for ( int j = i + 1; j < n; j++){
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = temp;
+			}
+    }
+}
---
+++ src/test/java/jalgor_gen2/leetcode/Q048Test_RotateImage.java
@@ -0,0 +1,33 @@
+package jalgor_gen2.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Q048Test_RotateImage extends AbstractTest<int[][]> {
+
+	private Q048_RotateImage sol = new Q048_RotateImage();
+	
+	
+	@Override
+	protected void baseTest(int[][] expected, int[][] actual) {
+		Assert.assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void test1(){
+		int[][] input = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		int[][] output = {{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+		sol.rotate(input);
+		this.baseTest(output, input);
+	}
+	
+	
+	@Test
+	public void test2(){
+		int[][] input = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+		int[][] output = {{15, 13, 2, 5}, {14, 3, 4, 1}, {12, 6, 8, 9}, {16, 7, 10, 11}};
+		sol.rotate(input);
+		this.baseTest(output, input);
+	}
+
+}
