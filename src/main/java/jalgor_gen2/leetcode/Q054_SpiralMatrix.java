b15c86146f3a923654d230565e637e3855b8355c
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2017-10-14 10:30:53 -0700

    q 054 spiral matrix

---
+++ src/main/java/jalgor_gen2/leetcode/Q054_SpiralMatrix.java
@@ -0,0 +1,30 @@
+package jalgor_gen2.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q054_SpiralMatrix extends Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+    	int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    	List<Integer> res = new ArrayList<Integer>();
+    	if (matrix.length == 0) return res;
+    	
+    	int m = matrix.length, n = matrix[0].length;
+    	int[] steps = {n, m-1};
+    	int iDir = 0, ir = 0, ic = -1;
+    	
+    	while (steps[iDir % 2] > 0){
+    		
+    		for (int i = 0; i < steps[iDir%2]; i++){
+    			ir += dirs[iDir][0];
+    			ic += dirs[iDir][1];
+    			res.add(matrix[ir][ic]);
+    		}
+    		
+    		steps[iDir % 2] --;
+    		iDir = (iDir + 1) % 4;
+    	}
+    	return res;
+    }
+}
---
+++ src/test/java/jalgor_gen2/leetcode/Q054Test_SpiralMatrix.java
@@ -0,0 +1,27 @@
+package jalgor_gen2.leetcode;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Q054Test_SpiralMatrix extends AbstractTest<List<Integer>> {
+
+	private Q054_SpiralMatrix sol = new Q054_SpiralMatrix();
+	
+	@Override
+	protected void baseTest(List<Integer> expected, List<Integer> actual) {
+		Assert.assertThat(actual, equalTo(expected));
+	}
+
+	@Test
+	public void test1(){
+		int[][] matrix = {{1,2,3},{4, 5, 6}, {7,8,9}};
+		List<Integer> output = Arrays.asList(new Integer[]{1,2,3,6,9,8,7,4,5});
+		this.baseTest(output, sol.spiralOrder(matrix));
+	}
+	
+}
