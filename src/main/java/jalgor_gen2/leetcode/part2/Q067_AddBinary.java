d6d39c31c8673f54300e482fd10855fcfbd3354c
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2018-01-10 11:29:22 +0800

    Q067 Add Binary

--- src/main/java/jalgor_gen2/leetcode/part2/Q067_AddBinary.java
+++ src/main/java/jalgor_gen2/leetcode/part2/Q067_AddBinary.java
@@ -4,7 +4,19 @@ public class Q067_AddBinary {
 
     public String addBinary(String a, String b) {
 
-
+	StringBuilder sb = new StringBuilder();
+	int m = a.length(), n = b.length(), carryOn = 0;
+	
+	for ( int i = m - 1, j = n - 1; i >= 0 || j >= 0; i--, j--) {
+	    int aVal = i < 0 ? 0 : a.charAt(i) - '0';
+	    int bVal = j < 0 ? 0 : b.charAt(j) - '0';
+	    sb.append((aVal + bVal + carryOn) % 2 );
+	    carryOn = (aVal + bVal + carryOn) / 2;
+	}
+	
+	if ( carryOn > 0) sb.append(carryOn);
+	
+	return sb.reverse().toString();
     }
     
 }
