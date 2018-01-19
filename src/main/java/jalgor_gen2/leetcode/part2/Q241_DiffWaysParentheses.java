d8d4ae57259dcc1beea4b1dfc49151d966bcc7dc
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2018-01-18 16:36:21 -0800

    Q241 Diff ways to add Parentheses

--- src/main/java/jalgor_gen2/leetcode/part2/Q241_DiffWaysParentheses.java
+++ src/main/java/jalgor_gen2/leetcode/part2/Q241_DiffWaysParentheses.java
@@ -1,11 +1,48 @@
 package jalgor_gen2.leetcode.part2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Q241_DiffWaysParentheses {
 
     public List<Integer> diffWaysToCompute(String input) {
-        return null;
+        
+	List<Integer> res = new ArrayList<Integer>();
+	int val = 0; List<Integer> left = null, right = null;
+	
+	for ( int i = 0; i < input.length(); i ++) {
+	    
+	    char c = input.charAt(i);
+	    
+	    if ( c == '*' || c == '+' || c == '-' ) {
+		
+		val = 0;
+		left = diffWaysToCompute(input.substring(0, i));
+		right  = diffWaysToCompute(input.substring(i+1));
+		
+		for ( Integer lv : left) {
+		    for ( Integer rv : right) {
+			
+			switch(c) {
+			case '*':
+			    res.add(lv * rv);
+			    break;
+			case '+':
+			    res.add(lv + rv);
+			    break;
+			case '-':
+			    res.add(lv - rv);
+			    break;
+			}
+		    }
+		}
+	    } else 
+		val = val * 10 + c - '0';
+	}
+	
+	if ( res.size() == 0) res.add(val);
+	
+	return res;
     }
     
 }
